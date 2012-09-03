09c2c43ba40d21093e0971fb0d1b29945a6cf5f1
Andi Sidwell <andi@takkaria.org>
2012-09-03 18:37:01 +0100

    Add some form validation logic

--- index.php
+++ index.php
@@ -44,23 +44,83 @@ F3::route('GET /event_add', function() {
 	echo Template::serve("templates/event_add.html");
 });
 
-/*
-
 F3::route('POST /event_add', function() {
-	validate fields
-	if not valid:
-		throw back to user
 
-	else:
-		user = user record from email
+	$messages = array();
+	$parsed_date = NULL;
+	$parsed_time = NULL;
 
-		if user.banned:
-			reject outright
-		if user.approved:
-			validate straight away
-		if not user.validated:
-			send email asking to confirm email address
-}); */
+	/* XXX try spam filtering logic here? */
+
+	F3::input('title', function($value) use(&$messages) {
+		if (strlen($value) < 3)
+			$messages[] = "Title too short.";
+		else if (strlen($value) > 140)
+			$messages[] = "Title too long.";
+	});
+
+	F3::input('date', function($value) use(&$messages, &$parsed_date) {
+		$parsed_date = strptime($value, "%d-%m-%Y");
+		if (!$parsed_date)
+			$messages[] = "Invalid date.";
+	});
+
+	F3::input('time', function($value) use(&$messages, &$parsed_time) {
+		$parsed_time = strptime($value, "%H:%M");
+		if (!$parsed_time)
+			$messages[] = "Invalid time.";
+	});
+
+	F3::input('email', function($value) use(&$messages) {
+		if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+			$messages[] = "Invalid email address";
+		}
+	});
+
+	if (count($messages) > 0) {
+		F3::set("title", "Add an event");
+		F3::set('POST', F3::scrub($_POST));
+		F3::set('messages', $messages);
+		echo Template::serve("templates/event_add.html");
+	} else {
+		/* Add the time to the already-established date */
+		$parsed_date['tm_min'] = $parsed_time['tm_min'];
+		$parsed_date['tm_hour'] = $parsed_time['tm_hour'];
+
+		/* Find the user record */
+		$user = new Axon('users');
+		$user->load('email="' . $_POST['email'] . '"'); /* XXX potential injection attack */
+
+		if ($user->dry()) {
+			/* send email to confirm, save event */
+		} else if ($user->banned) {
+			/* reject request */
+		}
+
+		/* save event */
+
+		if ($user->approved) {
+			/* set event to approved straight away */
+		}
+	}
+
+/* CREATE TABLE events
+(
+	id INT auto_increment PRIMARY KEY,
+	
+	title TEXT,
+	date DATETIME,
+	location TEXT,
+	blurb TEXT,
+	facebook_id INT,
+	url_info TEXT,
+	
+	submitter_email TEXT,
+	approved BOOLEAN
+); */
+
+	}
+});
 
 /* admin routing... 
 

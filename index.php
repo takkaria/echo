99de582784bd6fff7c39854c5683bf82b654c904
Andi Sidwell <andi@takkaria.org>
2012-09-20 00:17:55 +0100

    Make a new class, Event, and refactor accordingly.

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 require_once 'lib/fatfree/lib/base.php';
 
+F3::set('DEBUG', 3);
+
 date_default_timezone_set('UTC');
 $options = parse_ini_file('echo.ini', true);
-
-function group_assoc($array, $key) {
-    $return = array();
-    foreach ($array as $v) {
-        $return[$v[$key]][] = $v;
-    }
-    return $return;
-}
 
 function spam_check() {
 	global $options;
@@ -28,18 +22,6 @@ function spam_check() {
 	}
 }
 
-function send_confirm_email($to, $approved_hash) {
-	global $options;
-
-	/* Send confirm email */
-	F3::set("approved_id", $approved_hash);
-	$message = Template::serve('templates/event_confirm_mail.txt');
-
-	$subject = $options['general']['name'] . ": Please confirm your event";
-	$headers = "From: " . $options['general']['email'];
-	mail($to, $subject, $message, $headers);
-}
-
 function reroute($where) {
 	F3::reroute($where);
 }
@@ -48,57 +30,139 @@ function admin_check() {
 	// XXX fill this in
 }
 
-function event_get($where, $single = FALSE) {
-	DB::sql("SELECT * FROM events WHERE " . $where . " ORDER BY date");
-	$r = F3::get('DB->result');
-	if ($single)
-		$r = $r[0];
-	return $r;
-}
+class Event {
+	public $id;
+	public $title;
+	public $datetime;
+	public $location;
+	public $blurb;
+	public $email;
+	public $approved;
 
-function event_parse_form_data(&$messages) {
-	F3::input('title', function($value) use(&$messages) {
-		if (strlen($value) < 3)
-			$messages[] = "Title too short.";
-		else if (strlen($value) > 140)
-			$messages[] = "Title too long.";
-	});
+	function __construct($id = NULL) {
+		if ($id) {
+			DB::sql("SELECT * FROM events WHERE id=" . $id . " ORDER BY date");
+			$r = F3::get('DB->result');
 
-	F3::input('date', function($value) use(&$messages, &$date) {
-		$date = DateTime::createFromFormat("Y-m-j", $value);
-		if (!$date)
-			$messages[] = "Invalid date.";
-	});
+			// Get the first result
+			$r = $r[0];
 
-	F3::input('time', function($value) use(&$messages, &$date) {
-		$time = date_parse_from_format("H:i", $value);
-		if ($time['error_count'] > 0)
-			$messages[] = "Invalid time.";
-		if ($date)
-			$date->setTime($time['hour'], $time['minute']);
-	});
-
-	F3::input('email', function($value) use(&$messages) {
-		if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-			$messages[] = "Invalid email address";
+			$this->id = $r['id'];
+			$this->title = $r['title'];
+			$this->datetime =
+					DateTime::createFromFormat("Y-m-j H:i", $r['date']);
+			$this->location = $r['location'];
+			$this->blurb = $r['blurb'];
+			$this->email = $r['email'];
+			$this->approved = $r['approved'];
 		}
-	});
+	}
 
-	/* XXX need to make sure location and blurb are provided */
+	static function load($where) {
+		$events = array();
 
-	if (count($messages) > 0)
-		return;
+		DB::sql("SELECT id FROM events WHERE " . $where . " ORDER BY date");
+		$r = F3::get('DB->result');
+		foreach ($r as $row) {
+			$events[] = new Event($row['id']);
+		}
 
-	$event = array();
-	$event['title'] = $_POST['title'];
-	$event['date'] = $date->format("Y-m-d H:i");
-	$event['location'] = $_POST['location'];
-	$event['blurb'] = $_POST['blurb'];
-	$event['email'] = $_POST['email'];
+		return $events;
+	}
+	
+	public function parse_form_data() {
+		$messages = array();
+		$self = $this;
 
-	return $event;
+		F3::input('title', function($value) use(&$self, &$messages) {
+			if (strlen($value) < 3)
+				$messages[] = "Title too short.";
+			else if (strlen($value) > 140)
+				$messages[] = "Title too long.";
+
+			// XXX de-capitalise
+
+			$self->title = $value;
+		});
+	
+		F3::input('date', function($value) use(&$self, &$messages, &$date) {
+			$self->datetime = DateTime::createFromFormat("Y-m-j", $value);
+			if (!$self->datetime)
+				$messages[] = "Invalid date.";
+		});
+	
+		F3::input('time', function($value) use(&$self, &$messages, &$date) {
+			$time = date_parse_from_format("H:i", $value);
+			if ($time['error_count'] > 0)
+				$messages[] = "Invalid time.";
+			if ($self->datetime)
+				$self->datetime->setTime($time['hour'], $time['minute']);
+		});
+	
+		F3::input('email', function($value) use(&$self, &$messages) {
+			if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+				$messages[] = "Invalid email address";
+			}
+			$self->email = $value;
+		});
+	
+		/* XXX need to make sure location and blurb are provided */
+		$this->location = $_POST['location'];
+		$this->blurb = $_POST['blurb'];
+
+		return $messages;
+	}
+
+	public function generate_approved() {
+		$this->approved = md5($this->email . rand());
+	}
+
+	public function send_confirm_mail() {
+		global $options;
+
+		F3::set("approved_id", $this->approved);
+		$message = Template::serve('templates/event_confirm_mail.txt');
+	
+		$subject = $options['general']['name'] . ": Please confirm your event";
+		$headers = "From: " . $options['general']['email'];
+		mail($this->email, $subject, $message, $headers);
+	}
+
+	public function save() {
+		$e = new Axon('events');
+
+		if ($this->id)
+			$e->load('id=' . $this->id);
+
+		$e->id = $this->id;
+		$e->title = $this->title;
+		$e->date = $this->datetime->format("Y-m-d H:i");
+		$e->location = $this->location;
+		$e->blurb = $this->blurb;
+		$e->email = $this->email;
+		$e->approved = $this->approved;
+
+		return $e->save();
+	}
 }
 
+function set_event_data_from_POST() {
+	F3::set('title', F3::scrub($_POST['title']));
+	F3::set('date', F3::scrub($_POST['date']));
+	F3::set('time', F3::scrub($_POST['time']));
+	F3::set('location', F3::scrub($_POST['location']));
+	F3::set('blurb', F3::scrub($_POST['blurb']));
+	F3::set('email', F3::scrub($_POST['email']));
+}
+
+function set_event_data_from_Event($event) {
+	F3::set('title', $event->title);
+	F3::set('date', $event->datetime->format("Y-m-d"));
+	F3::set('time', $event->datetime->format("H:i"));
+	F3::set('location', $event->location);
+	F3::set('blurb', $event->blurb);
+	F3::set('email', $event->email);
+}
 
 /* ---------- */
 
@@ -113,17 +177,12 @@ F3::route('GET /', function() {
 	$where = "date >= date('now', 'start of day') AND " .
 			"date <= date('now', 'start of day', '+7 days') AND " .
 			"approved == 0";
-	$results = event_get($where);
+	$results = Event::load($where);
 
-	foreach ($results as &$event) {
-		$ts = strtotime($event['date']);
+	$sorted = array();
+	foreach ($results as $e)
+		$sorted[$e->datetime->format("A e B")][] = $e;
 
-		$event['timestamp'] = $ts;
-		$event['time'] = strftime('%H:%M', $ts);
-		$event['date'] = strftime('%A %e %B', $ts);
-	}
-
-	$sorted = group_assoc($results, "date");
 	F3::set('events', $sorted);
 
 	/* Feed posts */
@@ -159,12 +218,12 @@ F3::route('GET /event/add', function() {
 F3::route('POST /event/add', function() {
 	spam_check();
 
-	$messages = array();
-	$event = event_parse_form_data($messages);
+	$event = new Event();
+	$messages = $event->parse_form_data();
 
 	if (count($messages) > 0) {
 		F3::set("title", "Add an event");
-		F3::set('POST', F3::scrub($_POST));
+		set_event_data_from_POST();
 		F3::set('messages', $messages);
 		echo Template::serve("templates/event_add.html");
 	} else {
@@ -179,14 +238,11 @@ F3::route('POST /event/add', function() {
 
 		/* XXX should check the event hasn't already been saved */
 		/* Make event to save */
-		$e = new Axon('events');
-		F3::set('event', $event);
-		$e->copyFrom('event');
-		$event->approved = md5($event->email . rand());
-		$e->save();
+		$event->generate_approved();
+		$event->save();
+		$event->send_confirm_mail();
 
-		send_confirm_email($event->email, $event->approved);
-		reroute("/");
+		reroute("/?msg=Event+submitted.+Please+check+your+email.");
 	}
 });
 
@@ -211,15 +267,8 @@ F3::route('GET /c/@id', function() {
 F3::route('GET /event/@id', function() {
 	admin_check();
 
-	$id = intval(F3::get('PARAMS.id'));
-	$event = event_get("id = " . $id, TRUE);
-
-	// Split date into date/time
-	list ($event['date'], $event['time']) = explode(" ", $event['date']);
-
-	F3::set('POST', $event);
-
-	F3::set("title", "Edit event");
+	$event = new Event(intval(F3::get('PARAMS.id')));
+	set_event_data_from_Event($event);
 	echo Template::serve("templates/event_add.html");
 });
 
@@ -229,21 +278,17 @@ F3::route('POST /event/@id', function() {
 
 	$id = intval(F3::get('PARAMS.id'));
 
-	$messages = array();
-	$event = event_parse_form_data($messages);
+	$event = new Event($id);
+	$messages = $event->parse_form_data($messages);
 
 	if (count($messages) > 0) {
 		F3::set("title", "Edit event");
-		F3::set('POST', F3::scrub($_POST));
+		set_event_data_from_POST();
 		F3::set('messages', $messages);
 		echo Template::serve("templates/event_add.html");
 	} else {
-		/* Make event to save */
-		$e = new Axon('events');
-		$e->load('id=' . $id);
-		F3::set('event', $event);
-		$e->copyFrom('event');
-		$e->save();
+		/* Load event, modify it, then save it */
+		$event->save();
 		reroute("/event/" . $id . "?msg=Event%20saved.");
 	}
 });

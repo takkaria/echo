551db1d9648b1012af95d1cdfacd81b0924007d4
Andi Sidwell <andi@takkaria.org>
2014-02-26 01:55:27 +0000

    Add event box viewing in event listing, unify embed and site calendars

--- js/src/echo.js
+++ js/src/echo.js
@@ -1,21 +1,80 @@
+var echo = {
+	registered: false,
+
+	register_clickoff: function() {
+		if (echo.registered) return;
+
+		echo.registered = true;
+		$(document).click(function(e) {
+			var box = $("#eventinfo");
+			if (box &&
+					e.pageX > box.offset().left &&
+					e.pageX < box.offset().left+box.outerWidth() &&
+					e.pageY > box.offset().top &&
+					e.pageY < box.offset().left+box.outerHeight()) {
+				return false;
+			}
+
+			box.detach();
+			return true;
+		});
+	},
+
+	show_event: function(id, ev) {
+		// Clean up previous boxes
+		$("#eventinfo").detach();
+
+		// Register click-off handler
+		echo.register_clickoff();
+
+		// Create us a new one
+		var html = '<div id="eventinfo" class="eventbox"></div>';
+		$('body').append(html);
+
+		// Request the info
+		$("#eventinfo").load("/event/" + id, function() {
+			var info = $("#eventinfo");
+			info.css('top', ev.pageY + "px");
+
+			/* Don't overflow the page */
+			if (ev.pageX + info.outerWidth() > $(window).width())
+				info.css('left', $(window).width() - info.outerWidth() - 10);
+			else
+				info.css('left', ev.pageX + "px");
+
+			/* Don't overflow the heightwise either */
+			if (ev.pageY + info.outerHeight() > $("#calendar").height())
+				info.css('top', $("#calendar").height() - info.outerHeight() - 10);
+			else
+				info.css('top', ev.pageY + "px");
+
+			$("#eventinfo .close").click(function() {
+				$("#eventinfo").detach();
+			});
+
+			info.show();
+		});
+	}
+};
+
 $(function() {
 
-$("button.ajax").click(function(e) {
-	var url = this.form.action;
-	var method = this.form.method;
+	$("button.ajax").click(function(e) {
+		var url = this.form.action;
+		var method = this.form.method;
 
-	$.ajax(url, {
-		type: method,
-		error: function(jqXHR, textstatus, error) {
-			alert("Error " + textstatus + ": " + error);
-		},
-		success: function() {
-			alert("Successful.");
-			document.location = document.location;
-		}
-	})
+		$.ajax(url, {
+			type: method,
+			error: function(jqXHR, textstatus, error) {
+				alert("Error " + textstatus + ": " + error);
+			},
+			success: function() {
+				alert("Successful.");
+				document.location = document.location;
+			}
+		})
 
-	return false;
-});
+		return false;
+	});
 
 });

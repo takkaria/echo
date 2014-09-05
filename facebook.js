6c594805c732a792580977dbc01990e35e88783e
Andi Sidwell <andi@takkaria.org>
2014-09-05 15:45:17 +0100

    Get date/times working

--- facebook.js
+++ facebook.js
@@ -12,11 +12,7 @@ var c = d.querySelector(".see_more_link");
 if (c)
 	c.click();
 
-var datetime = /(.*)T(.*)/.exec(d.querySelector("[itemprop=startDate]").getAttribute("content"));
 var d = new Date(/(.*)T(.*)/.exec(d.querySelector("[itemprop=startDate]").getAttribute("content")));
-// d now contains the right info.  just need to get it out...
-
-var date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
 
 // Make the actual URL.
 d.location.href = makeUrl("https://echomanchester.net/event/add", {
@@ -24,6 +20,6 @@ d.location.href = makeUrl("https://echomanchester.net/event/add", {
 	location: d.querySelectorAll("._5xhk")[1].innerText + ", " + d.querySelectorAll("._5xhp")[1].innerText,
 	blurb: d.querySelector("#event_description").innerText,
 	url: d.location.href,
-	date1: datetime[1],
-	time1: datetime[2],
+	date1: d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate(),
+	time1: d.getHours() + ":" + d.getMinutes(),
 });

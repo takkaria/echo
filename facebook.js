c34836aa09d291bc7a1dea17cce04c85dbdce509
Andi Sidwell <andi@takkaria.org>
2014-09-05 16:03:57 +0100

    Finish fixing Facebook script, closes #31

--- facebook.js
+++ facebook.js
@@ -12,7 +12,7 @@ var c = d.querySelector(".see_more_link");
 if (c)
 	c.click();
 
-var d = new Date(/(.*)T(.*)/.exec(d.querySelector("[itemprop=startDate]").getAttribute("content")));
+var dt = new Date(d.querySelector("[itemprop=startDate]").getAttribute("content"));
 
 // Make the actual URL.
 d.location.href = makeUrl("https://echomanchester.net/event/add", {
@@ -20,6 +20,6 @@ d.location.href = makeUrl("https://echomanchester.net/event/add", {
 	location: d.querySelectorAll("._5xhk")[1].innerText + ", " + d.querySelectorAll("._5xhp")[1].innerText,
 	blurb: d.querySelector("#event_description").innerText,
 	url: d.location.href,
-	date1: d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate(),
-	time1: d.getHours() + ":" + d.getMinutes(),
+	date1: dt.getFullYear() + "-" + dt.getMonth() + "-" + dt.getDate(),
+	time1: dt.getHours() + ":" + dt.getMinutes(),
 });

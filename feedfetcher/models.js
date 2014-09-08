b624961c9ceef9e70be769bb5cc942962a268b3c
Andi Sidwell <andi@takkaria.org>
2014-09-08 11:47:01 +0100

    Add more model information

--- feedfetcher/models.js
+++ feedfetcher/models.js
@@ -5,13 +5,42 @@ var events = new sequelize('', '', '', {
 	storage: '../db/events.sqlite'
 })
 
-var Event = events.define('event', {
-	title: sequelize.TEXT,
-	startdt: sequelize.DATE,
-	enddt: sequelize.DATE
+var feeds = new sequelize('', '', '', {
+	dialect: 'sqlite',
+	storage: '../db/feeds.sqlite'
+})
+
+exports.Event = events.define('event', {
+	id: { type: sequelize.INTEGER, primaryKey: true },
+	title: { type: sequelize.TEXT },
+	startdt: { type:  sequelize.DATE },
+	enddt: { type: sequelize.DATE },
+	location: { type: sequelize.TEXT },
+	blurb: { type: sequelize.TEXT },
+	url: { type: sequelize.TEXT },
+	type: { type: sequelize.TEXT },
+	cost: { type: sequelize.TEXT },
+	state: {
+		type: sequelize.ENUM,
+		values: [ 'submitted', 'approved', 'imported' ]
+	},
+	email: { type: sequelize.TEXT },
+	key: { type: sequelize.TEXT }
 }, {
 	timestamps: false,
+	createdAt: false,
 	underscored: true
 })
 
-exports.Event = Event
+exports.Post = feeds.define('post', {
+	id: { type: sequelize.TEXT, primaryKey: true },
+	title: { type: sequelize.TEXT },
+	link: { type: sequelize.TEXT },
+	title: { type: sequelize.TEXT },
+	date: { type: sequelize.DATE },
+	summary: { type: sequelize.TEXT },
+	image: { type: sequelize.TEXT },
+	feed_url: { type: sequelize.TEXT },
+	eventish: { type: sequelize.BOOLEAN },
+	hidden: { type: sequelize.INTEGER }
+})

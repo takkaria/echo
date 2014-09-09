4286d5b8d37fd61525193cbf926215c19be9f835
Andi Sidwell <andi@takkaria.org>
2014-09-09 12:05:34 +0100

    Refactor

--- feedfetcher/fetch.js
+++ feedfetcher/fetch.js
@@ -3,41 +3,49 @@ var models = require('./models')
 
 Event = models.Event
 
-function new_event(data) {
+function fetch_feed(params) {
+	var url = params.url;
+	var filter = params.filter;
+	var action = params.action;
 
-	if (!data.start || data.start < new Date()) return;
-
-	Event.find({ where: { importid: data.uid } }).success(function() {
-
-		var e = Event.build({
-			title: data.summary,
-			startdt: data.start,
-			enddt: data.end,
-			location: data.location,
-			blurb: data.description,
-			state: 'imported',
-			importid: data.uid
-		})
-
-		console.log(e.values)
-
-	//	e.save()
-
-	});
-}
-
-function fetch_feed(url) {
 	ical.fromURL(url, {}, function(err, data) {
 		for (var k in data) {
 			if (!data.hasOwnProperty(k)) continue;
-			new_event(data[k]);
+			if (!filter || !filter(data[k])) continue;
+			if (!data.start || data.start < new Date()) continue;
+
+			Event.find({ where: { importid: data.uid } }).success(function() {
+				action(data[k]);
+			});
 		}
 	})
+}
+
+
+function add_event(data) {
+
+	var e = Event.build({
+		title: data.summary,
+		startdt: data.start,
+		enddt: data.end,
+		location: data.location,
+		blurb: data.description,
+		state: 'imported',
+		importid: data.uid
+	})
+
+	console.log(e.values)
+
+//	e.save()
 }
 
 fetch_feed({
 	url: 'https://www.google.com/calendar/ical/7etn2k6kvovrugd1hapue7ghrc%40group.calendar.google.com/public/basic.ics',
 	filter: function(data) {
+		day = data.start.getDay() // 0 = Sunday, 1 = Monday, etc.
+		if (day == 1 || day == 2) return false; // Filter out private events on Monday and Tuesday
+
 		return true;
-	}
+	},
+	action: add_event
 })
